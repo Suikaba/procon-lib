a54405b9bfdd60a5228a89009d410b53ec14d488
Suikaba <suibaka@hotmail.co.jp>
2016-10-26 01:26:30 +0900

    add template

---
+++ template.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <set>
+#include <stack>
+#include <map>
+#include <string>
+#include <deque>
+#include <iomanip>
+#include <bitset>
+#include <complex>
+#include <sstream>
+#include <algorithm>
+#include <limits>
+#include <cmath>
+#include <utility>
+#include <tuple>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+
+template <typename T>
+constexpr T INF = "Undefined";
+template <>
+constexpr int INF<int> = 1e9;
+template <>
+constexpr ll INF<ll> = 1e18;
+template <>
+constexpr ld INF<ld> = 1e30;
+
+constexpr int mod = 1e9+7;
+constexpr ld eps = 1e-10;
+constexpr ld pi = acos(-1.0);
+
+#define DUMP(a) cout << #a << ": " << (a) << endl
+
+
+int main() {
+}
+
