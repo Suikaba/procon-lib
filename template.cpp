e8fee688383f279e18afe645c8c4b6bcd083c7d0
Suikaba <suibaka@hotmail.co.jp>
2016-10-29 11:32:07 +0900

    include <array>

--- template.cpp
+++ template.cpp
@@ -6,6 +6,7 @@
 #include <stack>
 #include <map>
 #include <string>
+#include <array>
 #include <deque>
 #include <iomanip>
 #include <bitset>
@@ -17,6 +18,7 @@
 #include <utility>
 #include <tuple>
 #include <numeric>
+#include <cassert>
 using namespace std;
 
 using ll = long long;
