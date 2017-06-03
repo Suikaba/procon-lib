f34b67efd3dfd2ab85ed3d518e87268401f16e0a
Suikaba <suibaka@hotmail.co.jp>
2017-06-03 23:35:59 +0900

    add fenwick_tree

---
+++ structure/fenwick_tree.hpp
@@ -0,0 +1,27 @@
+template <typename T>
+class fenwick_tree {
+public:
+    fenwick_tree(int n) : n(n), dat(n, 0) {}
+
+    void add(int i, T value) {
+        for(; i<n; i |= i + 1) {
+            dat[i] += value;
+        }
+    }
+
+    T sum(int i) const {
+        T res = 0;
+        for(; i>=0; i = (i & (i+1)) - 1) {
+            res += dat[i];
+        }
+        return res;
+    }
+    // [l, r)
+    T sum(int l, int r) const {
+        return sum(r-1) - sum(l-1);
+    }
+
+private:
+    const int n;
+    std::vector<T> dat;
+};
