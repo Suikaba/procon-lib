f8d8163779d5315af6d7eb278072301ab94e36b2
Suikaba <suibaka@hotmail.co.jp>
2017-06-19 21:54:59 +0900

    update
    bug fix: 3th arg of inner_product (0 -> 0.0)
    add isis_sps  (sphere -- segment)
    typedef long double
    output point

--- math/geometry3d.hpp
+++ math/geometry3d.hpp
@@ -1,8 +1,21 @@
-// isis = is intersected ...
+#include <bits/stdc++.h>
+using namespace std;
+
+using ld = long double;
+constexpr ld eps = 1e-8;
+
 
 namespace geometry3d {
 
-using point = std::array<long double, 3>;
+using std::abs; // important
+using point = std::array<ld, 3>;
+
+
+std::ostream& operator<<(std::ostream& os, point const& p) {
+    os << '(' << p[0] << ", " << p[1] << ", " << p[2] << ')';
+    return os;
+}
+
 
 class segment {
 public:
@@ -26,11 +39,14 @@ public:
 
 class sphere {
 public:
-    sphere(point p_, long double r_)
+    sphere()
+        : p({0, 0, 0}), r(0)
+    {}
+    sphere(point p_, ld r_)
         : p(p_), r(r_)
     {}
     point p;
-    long double r;
+    ld r;
 };
 
 
@@ -50,56 +66,67 @@ point operator+(point const& a, point const& b) {
     return ret;
 }
 
-point operator*(point const& a, long double b) {
+point operator*(point const& a, ld b) {
     point ret = a;
     for(int i=0; i<3; ++i) {
         ret[i] *= b;
     }
     return ret;
 }
-point operator*(long double a, point const& b) {
+point operator*(ld a, point const& b) {
     return b*a;
 }
 
-bool eq(long double a, long double b) {
+bool eq(ld a, ld b) {
     return (std::abs(a - b) < eps);
 }
 
-point make_point(long double a, long double b, long double c) {
+point make_point(ld a, ld b, ld c) {
     return point{{a, b, c}};
 }
 
-long double dot(point a, point b) {
-    return std::inner_product(std::begin(a), std::end(a), std::begin(b), 0);    
+ld dot(point a, point b) {
+    return std::inner_product(std::begin(a), std::end(a), std::begin(b), 0.0);    
 }
 
 point cross(point a, point b) {
     return make_point(a[1]*b[2] - a[2]*b[1], a[2]*b[0] - a[0]*b[2], a[0]*b[1] - a[1]*b[0]);
 }
 
-long double norm(point a) { // |a|^2
+ld norm(point a) { // |a|^2
     return dot(a, a);
 }
 
-long double dist(point a) { // |a|
+ld abs(point a) { // |a|
     return std::sqrt(dot(a, a));
 }
 
 
 // verified
 point proj(line l, point p) {
-    point p1, p2, ret; // p1 = p - l.a, p2 = l.a - l.b, ret = l.a + t*(l.a - l.b)
-    for(int i=0; i<3; ++i) {
-        p1[i] = p[i] - l.a[i];
-        p2[i] = l.a[i] - l.b[i];
-    }
-    long double t = dot(p1, p2) / norm(p2);
-    for(int i=0; i<3; ++i) {
-        ret[i] = l.a[i] + t*p2[i];
-    }
-    return ret;
+    ld t = dot(p - l.a, l.a - l.b) / norm(l.a - l.b);
+    return l.a + t*(l.a - l.b);
 }
 
+
+bool isis_sps(sphere const& sp, segment const& s) {
+    auto a = s.a - sp.p;
+    auto b = s.b - sp.p;
+    auto p = proj(line(s), sp.p) - sp.p;
+    if(abs(a) + eps < sp.r && abs(b) + eps < sp.r && abs(p) + eps < sp.r) { // contain
+        return false;
+    }
+    if(abs(p) > sp.r + eps) {
+        return false;
+    }
+    if(abs(a) + eps < sp.r || abs(b) + eps < sp.r) {
+        return true;
+    }
+    if(abs(abs(a - p) + abs(b - p) - abs(b - a)) < eps) {
+        return true;
+    }
+    return false;
+}
 
 } // namespace geometry3d
 
