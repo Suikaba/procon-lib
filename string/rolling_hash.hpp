a7a93052f8ea984dfdaab374dbe51d511c67a078
Suikaba <suibaka@hotmail.co.jp>
2017-07-11 16:06:03 +0900

    update rolling hash
    don't use __int128

--- string/rolling_hash.hpp
+++ string/rolling_hash.hpp
@@ -3,21 +3,21 @@ class rolling_hash {
 public:
     rolling_hash(std::string const& s)
         : n(s.size()),
-          mod({1000000000000037LL, 1000000000000091LL})
+          mod({999999937LL, 1000000007LL})
     {
         for(int i=0; i<2; ++i) {
             hs[i].assign(n+1, 0);
             p[i].assign(n+1, 1);
             for(int j=0; j<n; ++j) {
-                hs[i][j+1] = (hs[i][j] * (__int128)base + s[j]) % mod[i];
-                p[i][j+1] = p[i][j] * (__int128)base % mod[i];
+                hs[i][j+1] = (hs[i][j] * base + s[j]) % mod[i];
+                p[i][j+1] = p[i][j] * base % mod[i];
             }
         }
     }
 
     // (l, r]
     long long query(int l, int r, int i) {
-        return ((hs[i][r] - (__int128)hs[i][l]*p[i][r-l]) % mod[i] + mod[i]) % mod[i];
+        return ((hs[i][r] - hs[i][l]*p[i][r-l]) % mod[i] + mod[i]) % mod[i];
     }
 
 private:
