097dadb8906914fff3b9e92bb459354f60108d54
Suikaba <suibaka@hotmail.co.jp>
2017-08-28 00:52:58 +0900

    update rolling_hash

--- string/rolling_hash.hpp
+++ string/rolling_hash.hpp
@@ -1,23 +1,31 @@
 
+// 1-indexed
+// s[1...n]
 class rolling_hash {
 public:
     rolling_hash(std::string const& s)
         : n(s.size()),
           mod({999999937LL, 1000000007LL})
     {
-        for(int i=0; i<2; ++i) {
-            hs[i].assign(n+1, 0);
-            p[i].assign(n+1, 1);
-            for(int j=0; j<n; ++j) {
-                hs[i][j+1] = (hs[i][j] * base + s[j]) % mod[i];
-                p[i][j+1] = p[i][j] * base % mod[i];
+        for(int i = 0; i < 2; ++i) {
+            hs[i].resize(n + 1);
+            p[i].resize(n + 1);
+            hs[i][0] = 0;
+            p[i][0] = 1;
+            for(int j = 0; j < n; ++j) {
+                hs[i][j + 1] = (hs[i][j] * base + s[j]) % mod[i];
+                p[i][j + 1] = p[i][j] * base % mod[i];
             }
         }
     }
 
-    // (l, r]
-    long long query(int l, int r, int i) {
-        return ((hs[i][r] - hs[i][l]*p[i][r-l]) % mod[i] + mod[i]) % mod[i];
+    // s[i...]
+    long long query(int idx, int i) const {
+        return hs[i][idx];
+    }
+    // s[l + 1...r]
+    long long query(int l, int r, int i) const {
+        return ((hs[i][r] - hs[i][l] * p[i][r - l]) % mod[i] + mod[i]) % mod[i];
     }
 
 private:
@@ -28,3 +36,4 @@ private:
     const std::array<long long, 2> mod;
     static const long long base = 29LL;
 };
+
