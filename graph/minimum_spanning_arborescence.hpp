ac8a89883888ebdf03fa01ae7d7906aab36e2dc1
Suikaba <suibaka@hotmail.co.jp>
2017-09-17 03:34:41 +0900

    rename function

--- graph/minimum_spanning_arborescence.hpp
+++ graph/minimum_spanning_arborescence.hpp
@@ -1,3 +1,4 @@
+#include <bits/stdc++.h>
 
 using weight = int;
 constexpr weight INF = 1e9;
@@ -83,7 +84,7 @@ std::vector<std::vector<int>> build_graph(std::vector<std::vector<int>> const& g
 }
 
 
-weight minimum_spanning_arborescence(graph const& g, int root, weight sum = 0) {
+weight minimum_spanning_rooted_arborescence(graph const& g, int root, weight sum = 0) {
     int const n = g.size();
 
     std::vector<int> rev(n, -1), cost(n, INF);
@@ -126,6 +127,39 @@ weight minimum_spanning_arborescence(graph const& g, int root, weight sum = 0) {
         }
     }
 
-    return minimum_spanning_arborescence(ng, cmp[root], sum);
+    return minimum_spanning_rooted_arborescence(ng, cmp[root], sum);
+}
+
+// not verified
+weight maximum_branching(graph const& g) {
+    graph g2(g.size() + 1);
+    int const super = g.size();
+    for(int i = 0; i < (int)g.size(); ++i) {
+        for(auto& e : g[i]) {
+            add_edge(g2, e.from, e.to, -e.cost);
+        }
+        add_edge(g2, super, i, 0);
+    }
+    return -minimum_spanning_rooted_arborescence(g2, super);
+}
+
+// not verified
+weight minimum_spanning_arborescence(graph const& g) {
+    weight K = 1;
+    int const n = g.size();
+    for(int i = 0; i < n; ++i) {
+        for(auto& e : g[i]) {
+            K += std::abs(e.cost);
+        }
+    }
+
+    auto g2 = g;
+    for(int i = 0; i < n; ++i) {
+        for(auto& e : g2[i]) {
+            e.cost = K - e.cost;
+        }
+    }
+    
+    return K * (n - 1) - maximum_branching(g2);
 }
 
