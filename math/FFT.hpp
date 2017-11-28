c287ffd2304249910df6c526dd7b5974f6125399
Suikaba <suibaka@hotmail.co.jp>
2017-11-28 19:38:50 +0900

    verified FFT

--- math/FFT.hpp
+++ math/FFT.hpp
@@ -1,20 +1,19 @@
-
 constexpr double pi = std::acos(-1.0);
 
-using data = double;
+using data = std::complex<double>;
 
 // v のサイズは必ず 2 のべき乗であること．
-std::vector<std::complex<data>> fft_impl(std::vector<std::complex<data>> v, data theta) {
+std::vector<data> fft_impl(std::vector<data> v, double theta) {
     int const n = v.size();
     for(int m = n; m >= 2; m >>= 1) {
         int mh = m >> 1;
-        std::complex<data> theta_i(0, theta);
+        data theta_i(0, theta);
         for(int i = 0; i < mh; ++i) {
-            std::complex<data> w = std::exp((data)i * theta_i);
+            data w = std::exp((data)i * theta_i);
             for(int j = i; j < n; j += m) {
-                std::complex<data> x = v[j] - v[j + mh]; // (a_k - a_{k + N/2})
-                v[j] = v[j] + v[j + mh];                 // (a_k + a_{k + N/2})
-                v[j + mh] = w * x;                       // (a_k - a_{k + N/2}) * W_N^j
+                data x = v[j] - v[j + mh]; // (a_k - a_{k + N/2})
+                v[j] = v[j] + v[j + mh];   // (a_k + a_{k + N/2})
+                v[j + mh] = w * x;         // (a_k - a_{k + N/2}) * W_N^j
             }
         }
         theta *= 2;
@@ -29,24 +28,22 @@ std::vector<std::complex<data>> fft_impl(std::vector<std::complex<data>> v, data
     return v;
 }
 
-std::vector<std::complex<data>> fft(std::vector<std::complex<data>> v) {
-    return fft_impl(move(v), 2 * pi / v.size());
+std::vector<data> fft(std::vector<data> v) {
+    return fft_impl(std::move(v), 2 * pi / v.size());
 }
 
-std::vector<std::complex<data>> inverse_fft(std::vector<std::complex<data>> v) {
-    return fft_impl(move(v), - 2 * pi / v.size());
+std::vector<data> inverse_fft(std::vector<data> v) {
+    return fft_impl(std::move(v), -2 * pi / v.size());
 }
 
-std::vector<std::complex<data>> convolution(std::vector<std::complex<data>> x, std::vector<std::complex<data>> y) {
+std::vector<data> convolution(std::vector<data> x, std::vector<data> y) {
     int sz = 1;
     int t = x.size() + y.size() - 1;
     while(sz < t) {
         sz <<= 1;
     }
-    x.resize(sz);
-    y.resize(sz);
-    x = fft(std::move(x));
-    y = fft(std::move(y));
+    x.resize(sz); y.resize(sz);
+    x = fft(std::move(x)); y = fft(std::move(y));
     for(int i = 0; i < sz; ++i) {
         x[i] *= y[i];
     }
@@ -56,4 +53,3 @@ std::vector<std::complex<data>> convolution(std::vector<std::complex<data>> x, s
     }
     return x;
 }
-
