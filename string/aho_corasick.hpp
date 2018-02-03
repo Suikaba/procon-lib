6d22220a3f72f8c0b2b1d3e945bd00704b65ff2e
Suikaba <suibaka@hotmail.co.jp>
2018-02-03 17:53:56 +0900

    refactoring Aho-Corasick

--- string/aho_corasick.hpp
+++ string/aho_corasick.hpp
@@ -1,26 +1,37 @@
 
+struct alphabets { // example: lower letters
+    static int const size = 26;
+    static int convert(char c) {
+        assert('a' <= c && c <= 'z');
+        return c - 'a';
+    }
+    static int convert_inv(int i) {
+        assert(0 <= i && i < size);
+        return 'a' + i;
+    }
+};
+
+template <typename Alphabets>
 class aho_corasick {
     struct PMA {
+        int const id;
         PMA* fail;
         std::vector<std::unique_ptr<PMA>> next;
         std::vector<int> accept;
-
-        PMA()
-            : fail(), next(alphabets)
-        {}
+        PMA(int i) : id(i), fail(), next(Alphabets::size) {}
     };
 
 public:
     aho_corasick(std::vector<std::string> const& ts)
-        : K(ts.size()), root(std::make_unique<PMA>())
-    {
+    : K(ts.size()), root(std::make_unique<PMA>(0)) {
+        int idx = 1; // node id
         root->fail = root.get();
         for(int i = 0; i < K; ++i) {
-            PMA* t = root.get();
+            auto t = root.get();
             for(auto cc : ts[i]) {
-                int c = cc - alphabet_base;
+                int c = Alphabets::convert(cc);
                 if(!t->next[c]) {
-                    t->next[c] = std::make_unique<PMA>();
+                    t->next[c] = std::make_unique<PMA>(idx++);
                 }
                 t = t->next[c].get();
             }
@@ -28,26 +39,19 @@ public:
         }
 
         std::queue<PMA*> que;
-        for(int c = 0; c < alphabets; ++c) {
+        for(int c = 0; c < Alphabets::size; ++c) {
             if(root->next[c]) {
                 root->next[c]->fail = root.get();
                 que.push(root->next[c].get());
             }
         }
         while(!que.empty()) {
-            PMA* t = que.front();
+            auto t = que.front();
             que.pop();
-            for(int c = 0; c < alphabets; ++c) {
+            for(int c = 0; c < Alphabets::size; ++c) {
                 if(t->next[c]) {
                     que.push(t->next[c].get());
-                    PMA* r = t->fail;
-                    while(!r->next[c] && r != root.get()) {
-                        r = r->fail;
-                    }
-                    PMA* nxt = r->next[c].get();
-                    if(nxt == nullptr) { // root
-                        nxt = root.get();
-                    }
+                    auto nxt = transition(t->fail, Alphabets::convert_inv(c));
                     t->next[c]->fail = nxt;
                     for(auto ac : nxt->accept) {
                         t->next[c]->accept.push_back(ac);
@@ -57,19 +61,20 @@ public:
         }
     }
 
-    // マッチングと同時に何かしたいならここをいじること
+    PMA* transition(PMA* node, char cc) {
+        int c = Alphabets::convert(cc);
+        auto now = node;
+        while(!now->next[c] && now != root.get()) now = now->fail;
+        now = now->next[c].get();
+        if(now == nullptr) now = root.get();
+        return now;
+    }
+
     std::vector<std::vector<int>> match(std::string const& s) {
         std::vector<std::vector<int>> res(K);
         PMA* now = root.get();
         for(int i = 0; i < (int)s.size(); ++i) {
-            int c = s[i] - alphabet_base;
-            while(!now->next[c] && now != root.get()) {
-                now = now->fail;
-            }
-            now = now->next[c].get();
-            if(now == nullptr) {
-                now = root.get();
-            }
+            now = transition(now, s[i]);
             for(auto k : now->accept) {
                 res[k].push_back(i);
             }
@@ -77,11 +82,11 @@ public:
         return res;
     }
 
-private:
-    // アルファベットはもっと一般化しても良い気がする．
-    static int const alphabets = 26;      // a-z
-    static int const alphabet_base = 'a'; // a
-    int const K;
-    std::shared_ptr<PMA> root;
-};
+    PMA* get_root() const {
+        return root.get();
+    }
 
+private:
+    int const K;
+    std::unique_ptr<PMA> root;
+};
