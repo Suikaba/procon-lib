78ac2c73f3f965db4515bbb8be7625fd7a411c25
Suikaba <suibaka@hotmail.co.jp>
2018-06-15 16:43:11 +0900

    update spfa

--- graph/spfa.hpp
+++ graph/spfa.hpp
@@ -9,8 +9,8 @@ using edges = std::vector<edge>;
 using graph = std::vector<edges>;
 
 // O(VE)
-// when g has neg cycle, second -> false
-std::pair<std::vector<weight>, bool> spfa(graph const& g, int s) {
+// when g has neg cycle, result is empty
+std::vector<weight> spfa(graph const& g, int s) {
     constexpr weight inf = std::numeric_limits<weight>::max() / 2;
     const int n = g.size();
     std::vector<weight> d(n, inf);
@@ -21,27 +21,24 @@ std::pair<std::vector<weight>, bool> spfa(graph const& g, int s) {
     in_q[s] = true;
     ++count[s];
     bool neg_cycle = false;
-    [&]() {
-        while(!que.empty()) {
-            int v = que.front();
-            que.pop();
-            in_q[v] = false;
-            for(auto& e : g[v]) {
-                if(d[v] != inf && d[e.to] > d[v] + e.cost) {
-                    d[e.to] = d[v] + e.cost;
-                    if(!in_q[e.to]) {
-                        que.emplace(e.to);
-                        in_q[e.to] = true;
-                        ++count[e.to];
-                        if(count[e.to] >= n) {
-                            neg_cycle = true;
-                            return;
-                        }
+    while(!que.empty()) {
+        int v = que.front();
+        que.pop();
+        in_q[v] = false;
+        for(auto& e : g[v]) {
+            if(d[v] != inf && d[e.to] > d[v] + e.cost) {
+                d[e.to] = d[v] + e.cost;
+                if(!in_q[e.to]) {
+                    que.emplace(e.to);
+                    in_q[e.to] = true;
+                    ++count[e.to];
+                    if(count[e.to] >= n) {
+                        neg_cycle = true;
+                        return std::vector<weight>();
                     }
                 }
             }
         }
-    }();
-
-    return std::make_pair(d, !neg_cycle);
+    }
+    return d;
 }
