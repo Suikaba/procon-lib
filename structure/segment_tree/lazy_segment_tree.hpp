1edf129c07eeaf9aeed2937dc12d8de45175aadb
Suikaba <suibaka@hotmail.co.jp>
2018-09-04 04:38:57 +0900

    refactoring lazy segment tree

--- structure/segment_tree/lazy_segment_tree.hpp
+++ structure/segment_tree/lazy_segment_tree.hpp
@@ -1,4 +1,3 @@
-// example:
 // Range Update and Range Min (update val >= 0)
 struct RURM {
     using type1 = int; // data type (Monoid)
@@ -21,6 +20,28 @@ struct RURM {
     }
 };
 
+// Range Update Range Sum
+struct RURS {
+    struct type1 {
+        int val, size;
+        type1(int v, int s) : val(v), size(s) {}
+    };
+    using type2 = int;
+
+    static type1 id1() { return type1(0, 0); } // initialize: seg<RURS>(n, type1(0, 1))
+    static type2 id2() { return std::numeric_limits<type2>::min() / 2; }
+
+    static type1 op1(type1 const& l, type1 const& r) {
+        return type1(l.val + r.val, l.size + r.size);
+    }
+    static type1 op2(type1 const& l, type2 const& r) {
+        return r != id2() ? type1(r * l.size, l.size) : l;
+    }
+    static type2 op3(type2 const& l, type2 const& r) {
+        return r != id2() ? r : l;
+    }
+};
+
 
 template <typename Monoid>
 class lazy_segment_tree {
@@ -30,7 +51,7 @@ class lazy_segment_tree {
 public:
     lazy_segment_tree(std::vector<T1> const& init)
         : sz(init.size()), n(expand(init.size())), h(std::ceil(std::log2(init.size()))),
-          data(n * 2), lazy(n * 2, Monoid::id2())
+          data(n * 2, Monoid::id1()), lazy(n * 2, Monoid::id2())
     {
         std::copy(std::begin(init), std::end(init), std::begin(data) + n);
         for(int i = n - 1; i >= 1; --i) {
@@ -42,11 +63,10 @@ public:
     {}
 
     T1 query(int l, int r) {
-        l += n, r += n - 1;
+        l += n, r += n;
         for(int i = h; i > 0; --i) {
             push(l >> i), push(r >> i);
         }
-        r++;
         T1 res1 = Monoid::id1(), res2 = Monoid::id1();
         while(l < r) {
             if(l & 1) res1 = Monoid::op1(res1, Monoid::op2(data[l], lazy[l])), ++l;
@@ -58,12 +78,11 @@ public:
 
     void update(int l, int r, T2 val) {
         assert(0 <= l && l <= r && r <= sz);
-        l += n, r += n - 1;
+        l += n, r += n;
         for(int i = h; i > 0; --i) {
             push(l >> i), push(r >> i);
         }
         int tl = l, tr = r;
-        r++;
         while(l < r) {
             if(l & 1) lazy[l] = Monoid::op3(lazy[l], val), ++l;
             if(r & 1) --r, lazy[r] = Monoid::op3(lazy[r], val);
@@ -76,9 +95,9 @@ public:
     }
 
 private:
-    int expand(int n) const {
+    int expand(int n_) const {
         int res = 1;
-        while(res < n) res <<= 1;
+        while(res < n_) res <<= 1;
         return res;
     }
 
@@ -93,13 +112,13 @@ private:
     }
 
     void update(int node) {
+        if(node * 2 + 1 >= (int)data.size()) return; // for n == max_r
         data[node] = Monoid::op1(Monoid::op2(data[node * 2], lazy[node * 2]),
                                  Monoid::op2(data[node * 2 + 1], lazy[node * 2 + 1]));
     }
 
 private:
-    int const sz;
-    int const n, h;
+    int const sz, n, h;
     std::vector<T1> data;
     std::vector<T2> lazy;
 };
