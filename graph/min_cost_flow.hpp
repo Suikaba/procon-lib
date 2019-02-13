408c2a0750cad20157bbb613c3832f8508e83323
Suikaba <suibaka@hotmail.co.jp>
2019-02-14 08:56:54 +0900

    refactor: min cost flow

--- graph/min_cost_flow.hpp
+++ graph/min_cost_flow.hpp
@@ -1,64 +1,50 @@
-using weight = int;
 
-struct edge {
-    int to, cap;
-    weight cost;
-    int rev;
-};
-
-using edges = std::vector<edge>;
-using graph = std::vector<edges>;
-
-void add_edge(graph& g, int from, int to, int cap, weight cost) {
-    g[from].push_back(edge{to, cap, cost, (int)g[to].size()});
-    g[to].push_back(edge{from, 0, -cost, (int)g[from].size()-1});
-}
-
-weight min_cost_flow(graph& g, int s, int t, weight f) {
-    using P = std::pair<weight, int>;
-    weight res = 0;
-    std::vector<weight> h(g.size(), 0);
-    std::vector<weight> dist(g.size());
+template <typename Edge,
+          typename Capacity = typename Edge::capacity_type,
+          typename Cost = typename Edge::cost_type>
+Cost min_cost_flow(graph<Edge>& g, int s, int t, Capacity f) {
+    using P = std::pair<Cost, int>;
+    const Cost inf = std::numeric_limits<Cost>::max() / 2;
+    Cost res = 0;
+    std::vector<Cost> h(g.size()), dist(g.size());
     std::vector<int> prevv(g.size()), preve(g.size());
     while(f > 0) {
-        std::priority_queue<P, std::vector<P>, std::greater<P>> que;
-        std::fill(dist.begin(), dist.end(), INF);
+        std::priority_queue<P, std::vector<P>, std::greater<>> que;
+        std::fill(std::begin(dist), std::end(dist), inf);
         dist[s] = 0;
-        que.push(P{dist[s], s});
+        que.emplace(dist[s], s);
         while(!que.empty()) {
-            P p = que.top(); que.pop();
-            int v = p.second;
-            if(dist[v] < p.first) {
-                continue;
-            }
+            const auto cur_d = que.top().first;
+            const int v = que.top().second;
+            que.pop();
+            if(dist[v] < cur_d) continue;
             for(int i = 0; i < (int)g[v].size(); ++i) {
-                edge& e = g[v][i];
+                auto& e = g[v][i];
                 if(e.cap > 0 && dist[e.to] > dist[v] + e.cost + h[v] - h[e.to]) {
                     dist[e.to] = dist[v] + e.cost + h[v] - h[e.to];
                     prevv[e.to] = v;
                     preve[e.to] = i;
-                    que.push(P{dist[e.to], e.to});
+                    que.emplace(dist[e.to], e.to);
                 }
             }
         }
-        if(dist[t] == INF) {
-            return -1;
-        }
+        if(dist[t] == inf) return -1;
         for(int v = 0; v < (int)g.size(); ++v) {
             h[v] += dist[v];
         }
 
-        weight d = f;
+        auto d = f;
         for(int v = t; v != s; v = prevv[v]) {
-            d = min(d, g[prevv[v]][preve[v]].cap);
+            d = std::min(d, g[prevv[v]][preve[v]].cap);
         }
         f -= d;
         res += d * h[t];
         for(int v = t; v != s; v = prevv[v]) {
-            edge& e = g[prevv[v]][preve[v]];
+            auto& e = g[prevv[v]][preve[v]];
             e.cap -= d;
             g[v][e.rev].cap += d;
         }
     }
     return res;
 }
+
