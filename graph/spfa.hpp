a5a9b6e1873c3ac0c678a8d74de5e0659b9fa46f
Suikaba <suibaka@hotmail.co.jp>
2019-02-14 05:36:48 +0900

    refactor: spfa

--- graph/spfa.hpp
+++ graph/spfa.hpp
@@ -1,44 +1,36 @@
-using weight = int;
 
-struct edge {
-    int to;
-    weight cost;
-};
-
-using edges = std::vector<edge>;
-using graph = std::vector<edges>;
-
-// O(VE)
-// when g has neg cycle, result is empty
-std::vector<weight> spfa(graph const& g, int s) {
-    constexpr weight inf = std::numeric_limits<weight>::max() / 2;
-    const int n = g.size();
-    std::vector<weight> d(n, inf);
-    std::vector<int> in_q(n), count(n);
+// Shortest Path Faster Algorithm.
+// Solve single source shortest path problem.
+// @param g: weighted graph
+// @param s: start node
+// @param inf: value big enough to represent infty (default max() / 2)
+// @return d (foreach vertex i, d[i] is shortest distance from s).
+//         If g has negative cycle, d is empty.
+// @complexity O(VE)
+template <typename Edge, typename Cost = typename Edge::cost_type>
+std::vector<Cost> spfa(graph<Edge> const& g, int s, Cost inf = std::numeric_limits<Cost>::max() / 2) {
+    std::vector<Cost> d(g.size(), inf);
+    std::vector<int> in_q(g.size()), count(g.size());
     std::queue<int> que;
-    d[s] = 0;
-    que.emplace(s);
+    d[s] = Cost(0);
     in_q[s] = true;
-    ++count[s];
-    bool neg_cycle = false;
+    count[s] = 1;
+    que.push(s);
     while(!que.empty()) {
-        int v = que.front();
+        const int v = que.front();
         que.pop();
         in_q[v] = false;
-        for(auto& e : g[v]) {
-            if(d[v] != inf && d[e.to] > d[v] + e.cost) {
-                d[e.to] = d[v] + e.cost;
-                if(!in_q[e.to]) {
-                    que.emplace(e.to);
-                    in_q[e.to] = true;
-                    ++count[e.to];
-                    if(count[e.to] >= n) {
-                        neg_cycle = true;
-                        return std::vector<weight>();
-                    }
-                }
+        for(const auto& e : g[v]) {
+            if(d[e.to] <= d[v] + e.cost) continue;
+            d[e.to] = d[v] + e.cost;
+            if(in_q[e.to]) continue;
+            que.emplace(e.to);
+            in_q[e.to] = true;
+            if(++count[e.to] >= static_cast<int>(g.size())) {
+                return std::vector<Cost>{};
             }
         }
     }
     return d;
 }
+
