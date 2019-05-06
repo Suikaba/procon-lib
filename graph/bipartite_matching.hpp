02add3ff647d2919bd108ec92f4bcd7f9f31ca26
Suikaba <suibaka@hotmail.co.jp>
2019-05-07 04:05:40 +0900

    perf: bipartite matching

--- graph/bipartite_matching.hpp
+++ graph/bipartite_matching.hpp
@@ -1,31 +1,49 @@
 using graph = std::vector<std::vector<int>>;
 
-bool dfs(graph const& g, int v, std::vector<int>& match, std::vector<bool>& used) {
-    used[v] = true;
-    for(auto u : g[v]) {
-        int w = match[u];
-        if(w < 0 || !used[w] && dfs(g, w, match, used)) {
-            match[v] = u;
-            match[u] = v;
-            return true;
-        }
-    }
-    return false;
-}
+// require: input graph is bipartite
+class bipartite_matching {
+public:
+    bipartite_matching(graph const& g)
+        : g_(g), match_(g.size(), -1), used_(g.size(), false)
+    {}
 
-// 入力は2部グラフである必要がある
-int bipartite_matching(graph const& g) {
-    const int V = g.size();
-    std::vector<int> match(V, -1);
-    std::vector<bool> used(V);
-    int res = 0;
-    for(int v = 0; v < V; ++v) {
-        if(match[v] < 0) {
-            std::fill(used.begin(), used.end(), false);
-            if(dfs(g, v, match, used)) {
-                ++res;
+    int solve() {
+        const int n = g_.size();
+        int res = 0;
+        bool update = true;
+        while(update) {
+            update = false;
+            for(int v = 0; v < n; ++v) {
+                if(match_[v] == -1 && dfs(v)) {
+                    update = true;
+                    ++res;
+                }
+            }
+            if(update) std::fill(used_.begin(), used_.end(), false);
+        }
+        return res;
+    }
+
+    int match(int v) const {
+        return match_[v];
+    }
+
+private:
+    bool dfs(int v) {
+        if(used_[v]) return false;
+        used_[v] = true;
+        for(auto u : g_[v]) {
+            if(match_[u] < 0 || dfs(match_[u])) {
+                match_[u] = v;
+                match_[v] = u;
+                return true;
             }
         }
+        return false;
     }
-    return res;
-}
+
+private:
+    graph g_;
+    std::vector<int> match_;
+    std::vector<bool> used_;
+};
